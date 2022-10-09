f7d7e99173fc7143641d4e00d6104b42cbfc4c76
meirbank <meir@meirbank.com>
2022-10-08 19:47:25 -0500

    NFT deployed

---
+++ nft/src/NFT.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.10;
+
+import "solmate/tokens/ERC721.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract NFT is ERC721 {
+    uint256 public currentTokenId;
+
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC721(_name, _symbol) {}
+
+    function mint() public payable returns (uint256) {
+        uint256 newItemId = ++currentTokenId;
+        _safeMint(msg.sender, newItemId);
+        return newItemId;
+    }
+
+    function tokenURI(uint256 id) public view virtual override returns (string memory) {
+        return Strings.toString(id);
+    }
+}
